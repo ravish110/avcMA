1c3a3017fb5b100bd4b7a364cc0a92a6df77fa65
ravish110 <ravish.abbas94@gmail.com>
2023-10-31 07:01:58 +0000

    initialize app

---
+++ avc-ma/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>AVC MA</h1>
+  );
+}
+
+export default App;
---
+++ avc-ma/src/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCy35hvtYQ_XdK-LWSPbSL9xMV9e39S16E",
+  authDomain: "avcma-c1b6f.firebaseapp.com",
+  projectId: "avcma-c1b6f",
+  storageBucket: "avcma-c1b6f.appspot.com",
+  messagingSenderId: "50061980655",
+  appId: "1:50061980655:web:0f891671264a1cc1ecbc1a"
+};
+
+const app = initializeApp(firebaseConfig);
